c2f8dd7f4aa090add0e634ab3db1f2f946dbd440
Ariane Holford <aribomop@gmail.com>
2020-06-26 16:31:47 +0100

    re-do fix for mobile typography

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -22,7 +22,7 @@ const NavBar = ({ auth, profile }) => {
 
 				<div className="hide-on-med-and-up">
 					<Link to="/" className="left ">
-						<h5>memo plan</h5>
+						memo plan
 					</Link>
 					{links}
 				</div>
